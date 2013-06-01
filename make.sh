6d254073cd89bb6d92d0726b181d77066bbc9ae4
Yannick Scherer <yannick.scherer@gmail.com>
2013-06-01 15:51:46 +0200

    Upgraded bashing.dev

--- make.sh
+++ make.sh
@@ -2,6 +2,5 @@
 
 SELF=$(cd "$(dirname "$0")" && pwd)
 BASHING="$SELF/build/bashing.dev"
-OUT="$SELF/build/bashing-0.1.0-SNAPSHOT"
 
-$BASHING compile --compact -o "$OUT" && chmod u+x "$OUT"
+$BASHING uberbash
